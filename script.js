5d7882a21b4c20b78a935f0297295096b084385a
Ashley Rodriguez <rodash87@gmail.com>
2022-05-19 21:46:40 -0700

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+var startButton = document.getElementById("start-button")
+var quizContainerEL = document.getElementById("quiz-container")
+var questionEL = document.getElementById("question")
+var answerEL = document.getElementById("answer-buttons")
 
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+    console.log("started")
+    startButton.classList.add("hide")
+    quizContainerEL.classList.remove("hide")
+    nextQuestion()
+    
+}
+
+function nextQuestion () {
+    showQuestion()
+
+}
+
+function showQuestion(question){
+    questionEL.innerText = question.question
+
+}
+
+function selectAnswer() {
+
+}
+
+var questions = [
+    {
+        question: Placing two of these symbols in front of a line of code will comment the code out?,
+        answer:[
+            { text: /, correct: true },
+            { text: (, correct: false },
+            { text: -, correct: false },
+            { text: &, correct: false }
+
+        ]
+
+    }
+]
